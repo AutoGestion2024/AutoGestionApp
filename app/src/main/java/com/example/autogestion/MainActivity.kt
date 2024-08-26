0e8cd5c6a883ea07b79326b89e611a9fa724e0b2
Istomine <none@none.com>
2024-08-26 11:24:09 +0200

    demo

--- app/src/main/java/com/example/autogestion/MainActivity.kt
+++ app/src/main/java/com/example/autogestion/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     AutoGestionTheme {
-        Greeting("Android")
+        Greeting("Hello, demo!")
     }
 }
