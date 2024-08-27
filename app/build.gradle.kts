b30fbeff55250479cf72f2e167b97b7f6d467908
GuilhermePintodac <guilherme.pintodacunhadamata@heig-vd.ch>
2024-08-27 19:08:09 +0200

    Search by plate number, client first name and client last name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.compose.material:material-icons-extended:1.0.5")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/autogestion/Home.kt
+++ app/src/main/java/com/example/autogestion/Home.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,6 +15,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -25,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,23 +51,30 @@ class ListPage : ComponentActivity() {
 
 @Composable
 fun MyApp1() {
-    val repair1 = Repair("Engine problem", "1633036800000")
-    val repair2 = Repair("Transmission issue", "1633123200000")
+    val repair1 = Repair("Engine problem", "16.08.2024")
+    val repair2 = Repair("Transmission issue", "13.04.2024")
+    val repair3 = Repair("Tyre replacement", "10.01.2024")
+    val repair4 = Repair("Oil change", "10.01.2024")
+    val repair5 = Repair("Service", "10.01.2024")
 
     val car1 = Car("ABC123", "Toyota", "Camry",1, listOf(repair1))
-    val car2 = Car("XYZ789", "Honda", "Civic", 1,listOf(repair2))
+    val car2 = Car("XYZ789", "Honda", "Civic", 1,listOf(repair2, repair3))
+    val car3 = Car("DEF789", "Mercedes", "A35 AMG", 2,listOf(repair4))
+    val car4 = Car("DEF789", "Volkswagen", "Cocinelle", 3,listOf(repair5))
 
-    val client = Client(1,"John", "Doe", listOf(car1, car2))
+    val client1 = Client(1,"John", "Doe", listOf(car1, car2))
+    val client2 = Client(2,"Lewis", "Hamilton", listOf(car3))
+    val client3 = Client(3,"Paul", "Albertini", listOf(car4))
 
 
     var searchText by remember { mutableStateOf(TextFieldValue("")) }
-    var items by remember { mutableStateOf(listOf("Item 1", "Item 2", "Item 3")) }
+    var items by remember { mutableStateOf(listOf(client1, client2, client3)) }
 
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    items = items + "Item ${items.size + 1}"
+                    // TODO: Redirect to the HomeForm page when it's ready
                 },
                 modifier = Modifier
                     .padding(16.dp)
@@ -77,16 +89,27 @@ fun MyApp1() {
                 .padding(padding)
         ) {
             Row (
-                horizontalArrangement = Arrangement.SpaceBetween,
+                horizontalArrangement = Arrangement.Start, // Align items horizontally
+                verticalAlignment = Alignment.CenterVertically, // Center vertically
+//                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ){
+
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(end = 8.dp)
+                )
+
                 // Barre de recherche
                 BasicTextField(
                     value = searchText,
                     onValueChange = { searchText = it },
                     modifier = Modifier
+                        .background(color = androidx.compose.ui.graphics.Color.LightGray)
                         .weight(1f)
                         .padding(end = 8.dp),
                     textStyle = LocalTextStyle.current.copy(fontSize = 18.sp)
@@ -97,7 +120,7 @@ fun MyApp1() {
                     }
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Search,
+                        imageVector = Icons.Default.CameraAlt,
                         contentDescription = null,
                         modifier = Modifier
                             .padding(end = 8.dp)
@@ -107,21 +130,32 @@ fun MyApp1() {
             }
 
             // Liste filtrée
-            val filteredItems = items.filter {
-                it.contains(searchText.text, ignoreCase = true)
+            val filteredItems = items.filter { client ->
+                client.firstName.contains(searchText.text, ignoreCase = true) ||
+                        client.lastName.contains(searchText.text, ignoreCase = true) ||
+                        client.cars.any { car ->
+                            car.plateNumber.contains(searchText.text, ignoreCase = true)
+                        }
             }
+
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
                 items(filteredItems.size) { index ->
+                    val client = filteredItems[index]
+                    val carsDescription = client.cars.joinToString(separator = "\n ") { "${it.make}, ${it.model}" }
                     Text(
-                        text = filteredItems[index],
+                        text = "${client.firstName} ${client.lastName}\n $carsDescription",
                         fontSize = 20.sp,
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(vertical = 8.dp)
+                            .clickable {
+                                println("${client.firstName} ${client.lastName}")
+                                // TODO: Navigate to the client's profile page when it's ready
+                            }
                     )
                 }
             }
