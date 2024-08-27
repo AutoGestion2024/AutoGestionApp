0380ad193a27b7cca5a6194072e24c73a37d47e7
Vitória Oliveira <mvitoriacoliveira@gmail.com>
2024-08-27 11:58:43 +0200

    add: db initialisation + launch using coroutine

--- app/src/main/java/com/example/autogestion/MainActivity.kt
+++ app/src/main/java/com/example/autogestion/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.autogestion
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +12,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.autogestion.data.Client
+import com.example.autogestion.data.ClientDatabase
 import com.example.autogestion.ui.theme.AutoGestionTheme
+import kotlinx.coroutines.*
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var database: ClientDatabase
+    // Coroutine scope for running database operations on the IO dispatcher.
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize the database
+        database = ClientDatabase.getDatabase(this)
+        // Launch a coroutine to add a client to the database.
+        coroutineScope.launch {
+            addClient()
+        }
+
+        // UI setup
         enableEdgeToEdge()
         setContent {
             AutoGestionTheme {
@@ -28,6 +46,21 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    // Function to add a client to the database.
+    private fun addClient() {
+        val newClient = Client(
+            clientId = 0,  // Auto-incremented
+            name = "John",
+            lastName = "Doe",
+            phone = "1234567890",
+            email = "john.doe@example.com",
+            address = "1234 Elm Street"
+        )
+        database.clientDao().addClient(newClient)
+        Log.d("MainActivity", "Client added")
+    }
+
 }
 
 @Composable
