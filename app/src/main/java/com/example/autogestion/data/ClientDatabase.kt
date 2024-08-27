552db5f0bb56aa62b1cb65e21886aa2c886f48d3
Vitória Oliveira <mvitoriacoliveira@gmail.com>
2024-08-27 11:57:41 +0200

    add: fct returning singleton instance of database

--- app/src/main/java/com/example/autogestion/data/ClientDatabase.kt
+++ app/src/main/java/com/example/autogestion/data/ClientDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.autogestion.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.autogestion.data.dao.ReparationDao
 import com.example.autogestion.data.dao.VehicleDao
@@ -15,5 +17,19 @@ abstract class ClientDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE: ClientDatabase? = null
+
+        // Function to get the singleton instance of the database.
+        fun getDatabase(context: Context): ClientDatabase {
+            // Return the existing instance if it exists or create a new one in a synchronized block.
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ClientDatabase::class.java,
+                    "appDB"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
     }
 }
