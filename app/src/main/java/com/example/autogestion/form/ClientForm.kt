8693f99da04f03a7b3769250ecb58f639f2860eb
CamilleKoestli <91135499+CamilleKoestli@users.noreply.github.com>
2024-09-02 11:44:09 +0200

    change format calendar, add address, add button save but not working

--- app/src/main/java/com/example/autogestion/form/ClientForm.kt
+++ app/src/main/java/com/example/autogestion/form/ClientForm.kt
@@ -6,16 +6,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.autogestion.Home
 import com.example.autogestion.NavBar
+import com.example.autogestion.data.Client
+import com.example.autogestion.data.viewModels.ClientViewModel
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+import android.app.DatePickerDialog
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarToday
+import java.util.Calendar
 
 
 class ClientForm : ComponentActivity() {
@@ -31,13 +43,15 @@ class ClientForm : ComponentActivity() {
         val phoneNumber = intent.getStringExtra("phoneNumber") ?: ""
         val birthDate = intent.getStringExtra("birthDate") ?: ""
         val email = intent.getStringExtra("email") ?: ""
+        val address = intent.getStringExtra("address") ?: ""
         setContent {
 
             ClientFormApp(initFirstName = firstName,
                 initLastName = lastName,
                 initPhoneNumber = phoneNumber,
                 initBirthDate = birthDate,
-                initEmail = email)
+                initEmail = email,
+                initAddress = address)
 
         }
     }
@@ -47,7 +61,10 @@ class ClientForm : ComponentActivity() {
                       initLastName: String,
                       initPhoneNumber: String,
                       initBirthDate: String,
-                      initEmail: String) {
+                      initEmail: String,
+                      initAddress: String,
+                      clientViewModel: ClientViewModel = viewModel()
+    ) {
         val context = LocalContext.current
 
         var firstName by remember { mutableStateOf(TextFieldValue(initFirstName)) }
@@ -55,6 +72,16 @@ class ClientForm : ComponentActivity() {
         var phoneNumber by remember { mutableStateOf(TextFieldValue(initPhoneNumber)) }
         var birthDate by remember { mutableStateOf(TextFieldValue(initBirthDate)) }
         var email by remember { mutableStateOf(TextFieldValue(initEmail)) }
+        var address by remember { mutableStateOf(TextFieldValue(initAddress)) }
+
+        var isFirstNameError by remember { mutableStateOf(false) }
+        var isLastNameError by remember { mutableStateOf(false) }
+        var isPhoneError by remember { mutableStateOf(false) }
+        var isBirthDateError by remember { mutableStateOf(false) }
+
+        val coroutineScope = rememberCoroutineScope()
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val calendar = Calendar.getInstance()
 
         Scaffold(
         ) { padding ->
@@ -91,6 +118,7 @@ class ClientForm : ComponentActivity() {
                     value = phoneNumber,
                     onValueChange = { phoneNumber = it },
                     label = { Text("Numéro de téléphone") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone),
                     modifier = Modifier.fillMaxWidth(),
                     isError = phoneNumber.text.isEmpty()
                 )
@@ -99,45 +127,126 @@ class ClientForm : ComponentActivity() {
                 OutlinedTextField(
                     value = birthDate,
                     onValueChange = { birthDate = it },
-                    label = { Text("Date de naissance (optional)") },
-                    modifier = Modifier.fillMaxWidth()
+                    label = { Text("Date de naissance (optionnel)") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.fillMaxWidth(),
+                    readOnly = true, // Make it readonly to open the date picker on click
+                    isError = isBirthDateError,
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            val datePickerDialog = DatePickerDialog(
+                                context,
+                                { _, year, month, dayOfMonth ->
+                                    calendar.set(year, month, dayOfMonth)
+                                    birthDate = TextFieldValue(dateFormat.format(calendar.time))
+                                    isBirthDateError = false
+                                },
+                                calendar.get(Calendar.YEAR),
+                                calendar.get(Calendar.MONTH),
+                                calendar.get(Calendar.DAY_OF_MONTH)
+                            )
+                            datePickerDialog.show()
+                        }) {
+                            Icon(Icons.Default.CalendarToday, contentDescription = "Select Date")
+                        }
+                    }
                 )
+                if (isBirthDateError) {
+                    Text("Format de date invalide", color = MaterialTheme.colorScheme.error)
+                }
                 Spacer(modifier = Modifier.height(16.dp))
 
                 OutlinedTextField(
                     value = email,
                     onValueChange = { email = it },
-                    label = { Text("Email (optional)") },
+                    label = { Text("Email (optionnel)") },
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.height(32.dp))
 
-                Button(
-                    onClick = {
-                        // TODO: Navigate to the CarForm screen
-                        val intent = Intent(context, VehicleForm::class.java).apply {
-                            putExtra("firstName", firstName.text)
-                            putExtra("lastName", lastName.text)
-                            putExtra("phoneNumber", phoneNumber.text)
-                            putExtra("birthDate", birthDate.text)
-                            putExtra("email", email.text)
-                        }
-                        context.startActivity(intent)
-                    },
-                    modifier = Modifier.align(Alignment.CenterHorizontally),
-                    enabled = firstName.text.isNotEmpty() && lastName.text.isNotEmpty() && phoneNumber.text.isNotEmpty()
+                OutlinedTextField(
+                    value = address,
+                    onValueChange = { address = it },
+                    label = { Text("Adresse (optionnel)") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Text("Suivant")
-                }
+                    Button(
+                        onClick = {
+                            val intent = Intent(context, VehicleForm::class.java).apply {
+                                putExtra("firstName", firstName.text)
+                                putExtra("lastName", lastName.text)
+                                putExtra("phoneNumber", phoneNumber.text)
+                                putExtra("birthDate", birthDate.text)
+                                putExtra("address", address.text)
+                            }
+                            context.startActivity(intent)
+                        },
+                        enabled = firstName.text.isNotEmpty() && lastName.text.isNotEmpty() && phoneNumber.text.isNotEmpty() && !isBirthDateError
+                    ) {
+                        Text("Suivant")
+                    }
+
+                    Button(
+                        onClick = {
+                            isFirstNameError = firstName.text.isEmpty()
+                            isLastNameError = lastName.text.isEmpty()
+                            isPhoneError = phoneNumber.text.isEmpty()
+
+                            if (!isFirstNameError && !isLastNameError && !isPhoneError && !isBirthDateError) {
+                                val birthDateLong = if (birthDate.text.isNotEmpty()) {
+                                    dateFormat.parse(birthDate.text)?.time ?: 0L
+                                } else {
+                                    null
+                                }
+                                val newClient = Client(
+                                    clientId = 0,  // Room will auto-generate the ID
+                                    lastName = lastName.text,
+                                    firstName = firstName.text,
+                                    phone = phoneNumber.text,
+                                    birthDate = birthDateLong ?: 0L,
+                                    email = email.text,
+                                    address = ""  // Assuming address field is not used in this form
+                                )
+
+                                // Add the client to the database and redirect to Home
+                                coroutineScope.launch {
+                                    clientViewModel.addClient(newClient)
+                                    redirectToHome(context)
+                                }
+                            }
+                        },
+                        enabled = firstName.text.isNotEmpty() && lastName.text.isNotEmpty() && phoneNumber.text.isNotEmpty() && !isBirthDateError
+                    ) {
+                        Text("Enregistrer")
+                    }
             }
         }
     }
+        }
+
+
 
     @Preview(showBackground = true)
     @Composable
     fun DefaultPreview2() {
-        ClientFormApp("","","","","")
+        ClientFormApp("","","","","","")
     }
 
 
 }
+
+    private fun redirectToHome(context: android.content.Context) {
+        val intent = Intent(context, Home::class.java)
+        context.startActivity(intent)
+        if (context is ComponentActivity) {
+            context.finish()
+        }
+    }
+
+
