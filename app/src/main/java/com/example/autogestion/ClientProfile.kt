b83c09dc7c4b7cbf023d212ef1c3c33ae9c8afc3
Vitória Oliveira <mvitoriacoliveira@gmail.com>
2024-09-05 14:58:10 +0200

    rfr: date formatting

--- app/src/main/java/com/example/autogestion/ClientProfile.kt
+++ app/src/main/java/com/example/autogestion/ClientProfile.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.autogestion.Utils.DateUtils
 import com.example.autogestion.Utils.NavigationUtils.navigateToClientFormUpdate
 import com.example.autogestion.Utils.NavigationUtils.navigateToHome
 import com.example.autogestion.Utils.NavigationUtils.navigateToVehicleFormAdd
@@ -94,7 +95,7 @@ class ClientProfile : ComponentActivity() {
 
                         // Formatting client's birthday for display
                         val formattedDate = currentClient.birthDate?.let {
-                            dateFormat.format(it)
+                            DateUtils.dateFormat.format(it)
                         } ?: "-"
 
                         // Display clients info
